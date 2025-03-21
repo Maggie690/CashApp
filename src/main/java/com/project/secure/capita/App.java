7e3c6f63b27d5910880171553bbe7ae0112d3608
m.ivanova <margaritawork76@gmail.com>
2025-03-21 08:00:28 +0200

    application.yaml, pom.xml, App - add BCryptPasswordEncoder

--- src/main/java/com/project/secure/capita/App.java
+++ src/main/java/com/project/secure/capita/App.java
@@ -2,12 +2,19 @@ package com.project.secure.capita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class App {
+    private static final int STRENGTH = 12;
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(STRENGTH);
+    }
 }
---
+++ src/main/java/com/project/secure/capita/exception/ApiException.java
@@ -0,0 +1,7 @@
+package com.project.secure.capita.exception;
+
+public class ApiException extends Exception{
+    public ApiException(String message) {
+        super(message);
+    }
+}
